11bdf0e82eadbaf874dd0286f569ac6ac6253a65
jonesbr <brandan.jones@uc.edu>
2018-11-27 19:03:01 -0500

    New example parsing vehicle JSON

---
+++ PlantPlaces18FS001/PlantPlaces18FS001/Vehicle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PlantPlaces18FS001
+{
+    public class Vehicle
+    {
+        private string asset;
+        private string distance_traveled;
+        private string heading;
+        private string latitude;
+        private string loadts;
+        private string longitude;
+        private string odometer;
+        private string reasons;
+        private string speed;
+        private string status;
+        private string time;
+
+        public string Asset { get => asset; set => asset = value; }
+        public string Distance_traveled { get => distance_traveled; set => distance_traveled = value; }
+        public string Heading { get => heading; set => heading = value; }
+        public string Latitude { get => latitude; set => latitude = value; }
+        public string Loadts { get => loadts; set => loadts = value; }
+        public string Longitude { get => longitude; set => longitude = value; }
+        public string Odometer { get => odometer; set => odometer = value; }
+        public string Reasons { get => reasons; set => reasons = value; }
+        public string Speed { get => speed; set => speed = value; }
+        public string Status { get => status; set => status = value; }
+        public string Time { get => time; set => time = value; }
+    }
+}
---
+++ PlantPlaces18FS001/PlantPlaces18FS001/VehicleTravel.aspx.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PlantPlaces18FS001
+{
+    public partial class VehicleTravel : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            using (var webClient = new WebClient())
+            {
+                String rawData =
+                    webClient.DownloadString("https://data.cincinnati-oh.gov/resource/w2ka-rfbi.json");
+
+                List<Vehicle> cityVehicles = JsonConvert.DeserializeObject<List<Vehicle>>(rawData);
+
+                int i = 1 + 1;
+               //  LblJSONCount.Text = "" + plantCollection.Plants.Count;
+            }
+
+            
+        }
+    }
+}
