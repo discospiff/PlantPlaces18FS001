9ca6eeebd419c932120c8376faec8214103a61cf
jonesbr <brandan.jones@uc.edu>
2018-11-27 20:37:51 -0500

    Add autocomplete with JSON source

---
+++ PlantPlaces18FS001/PlantPlaces18FS001/AutoCompletePlantNames.aspx.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PlantPlaces18FS001
+{
+    public partial class AutoCompletePlantNames : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Clear();
+            // change the content type, so the browser knows it's JSON
+            Response.ContentType = "application/json; charset=utf-8";
+
+            List<String> plantSuggestions = new List<String>();
+            plantSuggestions.Add("White Oak");
+            plantSuggestions.Add("Burr Oak");
+            plantSuggestions.Add("Red Oak");
+            plantSuggestions.Add("Pin Oak");
+
+            // change the list of plants to a JSON stream.
+            string plantJson = JsonConvert.SerializeObject(plantSuggestions);
+
+            // write our stuff!
+            Response.Write(plantJson);
+            // we're all done!
+            Response.End();
+        }
+    }
+}
